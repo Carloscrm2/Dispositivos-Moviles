c63f587b1f99651e3b56de7df8996660f5c1f5e4
Carlos Gómez García <cgomez.egarcia@gmail.com>
2022-10-06 09:38:30 -0500

    Se soluciono el problema de que la aplicacion se cerraba cuando se daba ENTER y se programo la funcion del ListenerOnKey desde una clase

--- ListenerOnKey/app/src/main/java/com/example/listeneronkey/MainActivity.java
+++ ListenerOnKey/app/src/main/java/com/example/listeneronkey/MainActivity.java
@@ -1,53 +1,76 @@
 package com.example.listeneronkey;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import org.w3c.dom.Text;
-
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    TextView txt1, txt2, txt3, txt4;
+    TextView tx1, tx2, tx3, tx4;
     EditText edtx1, edtx2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView txt1 = (TextView) findViewById(R.id.txt1);
-        TextView txt2 = (TextView) findViewById(R.id.txt2);
-        TextView txt3 = (TextView) findViewById(R.id.txt3);
-        TextView txt4 = (TextView) findViewById(R.id.txt4);
-        EditText edtx1 = (EditText) findViewById(R.id.edtx1);
-        EditText edtx2 = (EditText) findViewById(R.id.edtx2);
+        tx1 = (TextView) findViewById(R.id.txt1);
+        tx2 = (TextView) findViewById(R.id.txt2);
+        tx3 = (TextView) findViewById(R.id.txt3);
+        tx4 = (TextView) findViewById(R.id.txt4);
+        edtx1 = (EditText) findViewById(R.id.edtx1);
+        edtx2 = (EditText) findViewById(R.id.edtx2);
         Button button1 = (Button) findViewById(R.id.btn1);
-
         button1.setOnClickListener(this);
+        clasepropiaOnKeyListener miListener = new clasepropiaOnKeyListener();
+        edtx2.setOnKeyListener(miListener);
+        edtx1.setOnKeyListener(miListener);
+        //Poner teclado numerico
+        edtx1.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_VARIATION_PASSWORD);
 
-        edtx2.setOnKeyListener(new View.OnKeyListener() {
+       /*edtx2.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View view, int i, KeyEvent keyEvent) {
-                //Verificar si la tecla esta presionada y que sea la tecla ENTER
-                if (keyEvent.getAction()==KeyEvent.ACTION_DOWN && i==KeyEvent.KEYCODE_ENTER){
-                    txt3.setText("Tu nombre es: " + edtx1.getText().toString());
-                    txt4.setText("Tu apellido es: " + edtx2.getText().toString());
+            //Verificar si la tecla esta presionada y que sea la tecla ENTER
+                if(keyEvent.getAction()==KeyEvent.ACTION_DOWN && i==KeyEvent.KEYCODE_ENTER){
+                    tx3.setText("Tu nombre es: " + edtx1.getText().toString());
+                    tx4.setText("Tu apellido es: " + edtx2.getText().toString());
                     //Mensaje emergente
-                    Toast.makeText(getApplicationContext(), "Desde el OnKey", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(),"tecla = " + i,Toast.LENGTH_LONG).show();
                     return true;
                 }
                 return false;
             }
-        });
+        });*/
     }
-
     @Override
     public void onClick(View view) {
-        txt3.setText("Tu nombre es: " + edtx1.getText().toString());
-        txt4.setText("Tu apellido es: " + edtx2.getText().toString());
+        tx3.setText("Tu nombre es: " + edtx1.getText().toString());
+        tx4.setText("Tu apellido es: " + edtx2.getText().toString());
+        Toast.makeText(getApplicationContext(),"Desde el Botón",Toast.LENGTH_LONG).show();
     }
+    class clasepropiaOnKeyListener implements View.OnKeyListener{
+        @Override
+        public boolean onKey(View view, int i, KeyEvent keyEvent) {
+            if(keyEvent.getAction()==KeyEvent.ACTION_DOWN && i==KeyEvent.KEYCODE_ENTER){
+                switch(view.getId()){
+                    case R.id.edtx1:
+                        tx3.setText("Tu nombre es: " + edtx1.getText().toString());
+                        break;
+                    case R.id.edtx2:
+                        tx4.setText("Tu apellido es: " + edtx2.getText().toString());
+                        break;
+                }
+                //Mensaje emergente
+                Toast.makeText(getApplicationContext(),"tecla = " + i,Toast.LENGTH_LONG).show();
+                return true;
+            }
+            return false;
+        }
+    }
+
+
 }
