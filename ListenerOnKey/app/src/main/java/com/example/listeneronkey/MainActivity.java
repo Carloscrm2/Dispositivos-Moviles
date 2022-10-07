f7086f311ce8fd913f37160f74f241eb772921f9
Carlos Gómez García <cgomez.egarcia@gmail.com>
2022-10-07 09:56:33 -0500

    Se agregaron ciclos de vida de aplicacion y editor para guardar datos del usuario en un archivo

--- ListenerOnKey/app/src/main/java/com/example/listeneronkey/MainActivity.java
+++ ListenerOnKey/app/src/main/java/com/example/listeneronkey/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.listeneronkey;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,9 +16,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     TextView tx1, tx2, tx3, tx4;
     EditText edtx1, edtx2;
 
+    SharedPreferences misDatos;
+    String nombre, apellido;
+
+    CheckBox cbox1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Toast.makeText(getApplicationContext(),"onCreate",Toast.LENGTH_LONG).show();
         setContentView(R.layout.activity_main);
         tx1 = (TextView) findViewById(R.id.txt1);
         tx2 = (TextView) findViewById(R.id.txt2);
@@ -23,13 +32,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         tx4 = (TextView) findViewById(R.id.txt4);
         edtx1 = (EditText) findViewById(R.id.edtx1);
         edtx2 = (EditText) findViewById(R.id.edtx2);
+        cbox1 = (CheckBox) findViewById(R.id.cbox1);
         Button button1 = (Button) findViewById(R.id.btn1);
         button1.setOnClickListener(this);
         clasepropiaOnKeyListener miListener = new clasepropiaOnKeyListener();
         edtx2.setOnKeyListener(miListener);
         edtx1.setOnKeyListener(miListener);
         //Poner teclado numerico
-        edtx1.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_VARIATION_PASSWORD);
+        //edtx1.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_VARIATION_PASSWORD);
+
+        //getSharedPreferences("nombre archivo", modo: protegido(0))
+        misDatos = getSharedPreferences("preferencias", 0);
+        // El segundo parametro es por si no encuentra nada en el archivo
+        // El primer argumento es el nombre de la etiqueta
+        nombre = misDatos.getString("name", "No hay nombre");
+        apellido = misDatos.getString("last_name", "No hay apellido");
+        edtx1.setText(nombre);
+        edtx2.setText(apellido);
+
 
        /*edtx2.setOnKeyListener(new View.OnKeyListener() {
             @Override
@@ -46,6 +66,59 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });*/
     }
+    //Ciclos de vida de una aplicacion
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Toast.makeText(getApplicationContext(),"onStart",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Toast.makeText(getApplicationContext(),"onResume",Toast.LENGTH_LONG).show();
+    }
+
+    //Cuando la aplicacion esta en segundo plano
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Toast.makeText(getApplicationContext(),"onPause",Toast.LENGTH_LONG).show();
+        //Creamos un editor para poder guardar en el archivo los datos ingresados por el usuario
+        SharedPreferences.Editor miEditor = misDatos.edit();
+        nombre  = edtx1.getText().toString();
+        apellido = edtx2.getText().toString();
+        // Guardamos los datos ingresados en los EditText en el editor
+        miEditor.putString("name", nombre);
+        miEditor.putString("last_name", apellido);
+        // Guardamos el archivo
+        miEditor.commit();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Toast.makeText(getApplicationContext(),"onStop",Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        //Borramos los datos del archivo
+        if (cbox1.isChecked()==true){
+            misDatos.edit().clear().commit();
+            Toast.makeText(getApplicationContext(),"onDestroy",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Toast.makeText(getApplicationContext(),"onRestart",Toast.LENGTH_LONG).show();
+    }
+
+
     @Override
     public void onClick(View view) {
         tx3.setText("Tu nombre es: " + edtx1.getText().toString());
